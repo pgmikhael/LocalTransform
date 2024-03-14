7fb1be5a6d67e44d485a40d37313bf1109b2ea1a
Peter G. Mikhael <pgmikhael@rbgquanta1.csail.mit.edu>
2024-03-14 17:32:03 -0400

    add protein model

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -10,12 +10,14 @@ import dgl
 import dgl.backend as F
 from dgl.data.utils import save_graphs, load_graphs
 
+
 def combine_reactants(reactant, reagent):
-    if str(reagent) == 'nan':
+    if str(reagent) == "nan":
         smiles = reactant
     else:
-        smiles = '.'.join([reactant, reagent])
+        smiles = ".".join([reactant, reagent])
     return smiles
+
 
 def get_bonds(smiles):
     mol = Chem.MolFromSmiles(smiles)
@@ -32,132 +34,237 @@ def get_bonds(smiles):
         B += b
     V = []
     for a in A:
-        V += [(a,b) for b in A if a != b and (a,b) not in B]
+        V += [(a, b) for b in A if a != b and (a, b) not in B]
     return np.array(V), np.array(B)
-    
-def get_adm(mol, max_distance = 6):
+
+
+def get_adm(mol, max_distance=6):
     mol_size = mol.GetNumAtoms()
     distance_matrix = np.ones((mol_size, mol_size)) * max_distance + 1
     dm = Chem.GetDistanceMatrix(mol)
-    dm[dm > 100] = -1 # remote (different molecule)
-    dm[dm > max_distance] = max_distance # remote (same molecule)
+    dm[dm > 100] = -1  # remote (different molecule)
+    dm[dm > max_distance] = max_distance  # remote (same molecule)
     dm[dm == -1] = max_distance + 1
-    distance_matrix[:dm.shape[0],:dm.shape[1]] = dm
+    distance_matrix[: dm.shape[0], : dm.shape[1]] = dm
     return distance_matrix
-    
+
+
 class USPTODataset(object):
-    def __init__(self, args, mol_to_graph, node_featurizer, edge_featurizer, load=True, log_every=1000):
-        df = pd.read_csv('%s/labeled_data.csv' % args['data_dir'])
-        self.train_ids = df.index[df['Split'] == 'train'].values
-        self.val_ids = df.index[df['Split'] == 'valid'].values
-        self.test_ids = df.index[df['Split'] == 'test'].values
-        self.reactants = df['Reactants'].tolist()
-        self.reagents = df['Reagents'].tolist()
-        self.masks = df['Mask'].tolist()
-        
-        self.sep = args['sep']
-        self.fgraph_path = '../data/saved_graphs/full_%s_fgraph.bin' % args['dataset']
+    def __init__(
+        self,
+        args,
+        mol_to_graph,
+        node_featurizer,
+        edge_featurizer,
+        load=True,
+        log_every=1000,
+    ):
+        df = pd.read_csv("%s/labeled_data.csv" % args["data_dir"])
+        self.use_protein_sequence = args["use_protein_sequence"]
+        self.sequences = pickle.load(open("%s/sequences.p" % args["data_dir"], "rb"))
+        self.uniprot2sequences = pickle.load(
+            open(
+                "/Mounts/rbg-storage1/datasets/Enzymes/EnzymeMap/uniprot2sequence.p",
+                "rb",
+            )
+        )
+        self.train_ids = df.index[df["Split"] == "train"].values
+        self.val_ids = df.index[df["Split"] == "valid"].values
+        self.test_ids = df.index[df["Split"] == "test"].values
+        self.reactants = df["Reactants"].tolist()
+        self.reagents = df["Reagents"].tolist()
+        self.masks = df["Mask"].tolist()
+
+        self.sep = args["sep"]
+        self.fgraph_path = "../data/saved_graphs/full_%s_fgraph.bin" % args["dataset"]
         if self.sep:
-            self.labels = [eval(t) for t in df['Labels_sep']]
-            self.dgraph_path = '../data/saved_graphs/full_%s_dgraph_sep.pkl' % args['dataset']  # changed by numpy matrix for faster processing speed
+            self.labels = [eval(t) for t in df["Labels_sep"]]
+            self.dgraph_path = (
+                "../data/saved_graphs/full_%s_dgraph_sep.pkl" % args["dataset"]
+            )  # changed by numpy matrix for faster processing speed
         else:
-            self.labels = [eval(t) for t in df['Labels_mix']]
-            self.dgraph_path = '../data/saved_graphs/full_%s_dgraph_mix.pkl' % args['dataset']
+            self.labels = [eval(t) for t in df["Labels_mix"]]
+            self.dgraph_path = (
+                "../data/saved_graphs/full_%s_dgraph_mix.pkl" % args["dataset"]
+            )
         self._pre_process(mol_to_graph, node_featurizer, edge_featurizer)
-    
+
     def _pre_process(self, mol_to_graph, node_featurizer, edge_featurizer):
         self.fgraphs_exist = os.path.exists(self.fgraph_path)
         self.dgraphs_exist = os.path.exists(self.dgraph_path)
         self.fgraphs = []
         self.dgraphs = []
-                    
-        if not self.fgraphs_exist or not self.dgraphs_exist:   
-            for (s1, s2) in tqdm(zip(self.reactants, self.reagents), total=len(self.reactants), desc='Building dgl graphs...'):
+
+        if not self.fgraphs_exist or not self.dgraphs_exist:
+            for s1, s2 in tqdm(
+                zip(self.reactants, self.reagents),
+                total=len(self.reactants),
+                desc="Building dgl graphs...",
+            ):
                 smiles = combine_reactants(s1, s2)  # s1.s2
                 mol = Chem.MolFromSmiles(smiles)
                 if not self.fgraphs_exist:
-                    fgraph = mol_to_graph(mol, node_featurizer=node_featurizer, edge_featurizer=edge_featurizer, canonical_atom_order=False)
+                    fgraph = mol_to_graph(
+                        mol,
+                        node_featurizer=node_featurizer,
+                        edge_featurizer=edge_featurizer,
+                        canonical_atom_order=False,
+                    )
                     self.fgraphs.append(fgraph)
-                if  not self.dgraphs_exist:
+                if not self.dgraphs_exist:
                     if self.sep:
-                        self.dgraphs.append({'atom_distance_matrix': get_adm(mol), 'bonds':get_bonds(s1)})
+                        self.dgraphs.append(
+                            {
+                                "atom_distance_matrix": get_adm(mol),
+                                "bonds": get_bonds(s1),
+                            }
+                        )
                     else:
-                        self.dgraphs.append({'atom_distance_matrix': get_adm(mol), 'bonds':get_bonds(smiles)})
-                        
+                        self.dgraphs.append(
+                            {
+                                "atom_distance_matrix": get_adm(mol),
+                                "bonds": get_bonds(smiles),
+                            }
+                        )
+
         if self.fgraphs_exist:
-            print ('Loading feture graphs from %s...' % self.fgraph_path)
+            print("Loading feture graphs from %s..." % self.fgraph_path)
             self.fgraphs, _ = load_graphs(self.fgraph_path)
         else:
             save_graphs(self.fgraph_path, self.fgraphs)
-                
+
         if self.dgraphs_exist:
-            print ('Loading dense graphs from %s...' % self.dgraph_path)
-            with open(self.dgraph_path, 'rb') as f:
+            print("Loading dense graphs from %s..." % self.dgraph_path)
+            with open(self.dgraph_path, "rb") as f:
                 self.dgraphs = pickle.load(f)
         else:
-            with open(self.dgraph_path, 'wb') as f:
+            with open(self.dgraph_path, "wb") as f:
                 pickle.dump(self.dgraphs, f)
-            
+
     def __getitem__(self, item):
         dgraph = self.dgraphs[item]
-        dgraph['v_bonds'], dgraph['r_bonds'] = dgraph['bonds']
-        return self.reactants[item], self.reagents[item], self.fgraphs[item], dgraph, self.labels[item], self.masks[item]
+        dgraph["v_bonds"], dgraph["r_bonds"] = dgraph["bonds"]
+        if self.use_protein_sequence:
+            sequence = self.uniprot2sequences[self.sequences[item]]
+        else:
+            sequence = "<unk>"
+        return (
+            self.reactants[item],
+            self.reagents[item],
+            self.fgraphs[item],
+            dgraph,
+            self.labels[item],
+            self.masks[item],
+            sequence,
+        )
 
     def __len__(self):
-            return len(self.reactants)
-        
+        return len(self.reactants)
+
+
 class USPTOTestDataset(object):
-    def __init__(self, args, mol_to_graph, node_featurizer, edge_featurizer, load=True, log_every=1000):
-        df = pd.read_csv('%s/preprocessed_test.csv' % args['data_dir'])
-        self.reactants = df['Reactants'].tolist()
-        self.reagents = df['Reagents'].tolist()
-        self.sep = args['sep']
-        self.fgraph_path = '../data/saved_graphs/test_%s_fgraph.bin' % args['dataset']
+    def __init__(
+        self,
+        args,
+        mol_to_graph,
+        node_featurizer,
+        edge_featurizer,
+        load=True,
+        log_every=1000,
+    ):
+        df = pd.read_csv("%s/preprocessed_test.csv" % args["data_dir"])
+        self.use_protein_sequence = args["use_protein_sequence"]
+        self.sequences = pickle.load(
+            open("%s/test_sequences.p" % args["data_dir"], "rb")
+        )
+        self.uniprot2sequences = pickle.load(
+            open(
+                "/Mounts/rbg-storage1/datasets/Enzymes/EnzymeMap/uniprot2sequence.p",
+                "rb",
+            )
+        )
+        self.reactants = df["Reactants"].tolist()
+        self.reagents = df["Reagents"].tolist()
+        self.sep = args["sep"]
+        self.fgraph_path = "../data/saved_graphs/test_%s_fgraph.bin" % args["dataset"]
         if self.sep:
-            self.dgraph_path = '../data/saved_graphs/test_%s_dgraph_sep.pkl' % args['dataset']  # changed by numpy matrix for faster processing speed
+            self.dgraph_path = (
+                "../data/saved_graphs/test_%s_dgraph_sep.pkl" % args["dataset"]
+            )  # changed by numpy matrix for faster processing speed
         else:
-            self.dgraph_path = '../data/saved_graphs/test_%s_dgraph_mix.pkl' % args['dataset']
+            self.dgraph_path = (
+                "../data/saved_graphs/test_%s_dgraph_mix.pkl" % args["dataset"]
+            )
         self._pre_process(mol_to_graph, node_featurizer, edge_featurizer)
-        
-    
+
     def _pre_process(self, mol_to_graph, node_featurizer, edge_featurizer):
         self.fgraphs_exist = os.path.exists(self.fgraph_path)
         self.dgraphs_exist = os.path.exists(self.dgraph_path)
         self.fgraphs = []
         self.dgraphs = []
-                    
-        if not self.fgraphs_exist or not self.dgraphs_exist:   
-            for (s1, s2) in tqdm(zip(self.reactants, self.reagents), total=len(self.reactants), desc='Building dgl graphs...'):
+
+        if not self.fgraphs_exist or not self.dgraphs_exist:
+            for s1, s2 in tqdm(
+                zip(self.reactants, self.reagents),
+                total=len(self.reactants),
+                desc="Building dgl graphs...",
+            ):
                 smiles = combine_reactants(s1, s2)  # s1.s2
                 if not self.fgraphs_exist:
                     mol = Chem.MolFromSmiles(smiles)
-                    fgraph = mol_to_graph(mol, node_featurizer=node_featurizer, edge_featurizer=edge_featurizer, canonical_atom_order=False)
+                    fgraph = mol_to_graph(
+                        mol,
+                        node_featurizer=node_featurizer,
+                        edge_featurizer=edge_featurizer,
+                        canonical_atom_order=False,
+                    )
                     self.fgraphs.append(fgraph)
-                if  not self.dgraphs_exist:
+                if not self.dgraphs_exist:
                     if self.sep:
-                        self.dgraphs.append({'atom_distance_matrix': get_adm(mol), 'bonds':get_bonds(s1)})
+                        self.dgraphs.append(
+                            {
+                                "atom_distance_matrix": get_adm(mol),
+                                "bonds": get_bonds(s1),
+                            }
+                        )
                     else:
-                        self.dgraphs.append({'atom_distance_matrix': get_adm(mol), 'bonds':get_bonds(smiles)})
-                        
+                        self.dgraphs.append(
+                            {
+                                "atom_distance_matrix": get_adm(mol),
+                                "bonds": get_bonds(smiles),
+                            }
+                        )
+
         if self.fgraphs_exist:
-            print ('Loading feture graphs from %s...' % self.fgraph_path)
+            print("Loading feture graphs from %s..." % self.fgraph_path)
             self.fgraphs, _ = load_graphs(self.fgraph_path)
         else:
             save_graphs(self.fgraph_path, self.fgraphs)
-                
+
         if self.dgraphs_exist:
-            print ('Loading dense graphs from %s...' % self.dgraph_path)
-            with open(self.dgraph_path, 'rb') as f:
+            print("Loading dense graphs from %s..." % self.dgraph_path)
+            with open(self.dgraph_path, "rb") as f:
                 self.dgraphs = pickle.load(f)
         else:
-            with open(self.dgraph_path, 'wb') as f:
+            with open(self.dgraph_path, "wb") as f:
                 pickle.dump(self.dgraphs, f)
-            
+
     def __getitem__(self, item):
         dgraph = self.dgraphs[item]
-        dgraph['v_bonds'], dgraph['r_bonds'] = dgraph['bonds']
-        return self.reactants[item], self.reagents[item], self.fgraphs[item], dgraph
+        dgraph["v_bonds"], dgraph["r_bonds"] = dgraph["bonds"]
+        if self.use_protein_sequence:
+            uniprot_id = self.sequences[item]
+            sequence = self.uniprot2sequences.get(uniprot_id, "<unk>")
+        else:
+            sequence = "<unk>"
+
+        return (
+            self.reactants[item],
+            self.reagents[item],
+            self.fgraphs[item],
+            dgraph,
+            sequence,
+        )
 
     def __len__(self):
-            return len(self.reactants)
-        
+        return len(self.reactants)
--- scripts/models.py
+++ scripts/models.py
@@ -5,78 +5,211 @@ import sklearn
 import dgl
 import dgllife
 from dgllife.model import MPNNGNN
-from model_utils import pack_atom_feats, unpack_atom_feats, pack_bond_feats, unpack_bond_feats, reactive_pooling, Global_Reactivity_Attention, GELU
+from model_utils import (
+    pack_atom_feats,
+    unpack_atom_feats,
+    pack_bond_feats,
+    unpack_bond_feats,
+    reactive_pooling,
+    Global_Reactivity_Attention,
+    GELU,
+)
+
 
 class LocalTransform(nn.Module):
-    def __init__(self,
-                 node_in_feats,
-                 edge_in_feats,
-                 node_out_feats,
-                 edge_hidden_feats,
-                 num_step_message_passing,
-                 attention_heads,
-                 attention_layers,
-                 Template_rn, 
-                 Template_vn):
+    def __init__(
+        self,
+        node_in_feats,
+        edge_in_feats,
+        node_out_feats,
+        edge_hidden_feats,
+        num_step_message_passing,
+        attention_heads,
+        attention_layers,
+        Template_rn,
+        Template_vn,
+    ):
         super(LocalTransform, self).__init__()
-        
+
         self.activation = GELU()
-        
-        self.mpnn = MPNNGNN(node_in_feats=node_in_feats,
-                           node_out_feats=node_out_feats,
-                           edge_in_feats=edge_in_feats,
-                           edge_hidden_feats=edge_hidden_feats,
-                           num_step_message_passing=num_step_message_passing)
-        
-        self.atom_att = Global_Reactivity_Attention(node_out_feats, attention_heads, attention_layers, 8)
-        self.bond_att = Global_Reactivity_Attention(node_out_feats, attention_heads, attention_layers, 2)
-        
+
+        self.mpnn = MPNNGNN(
+            node_in_feats=node_in_feats,
+            node_out_feats=node_out_feats,
+            edge_in_feats=edge_in_feats,
+            edge_hidden_feats=edge_hidden_feats,
+            num_step_message_passing=num_step_message_passing,
+        )
+
+        self.atom_att = Global_Reactivity_Attention(
+            node_out_feats, attention_heads, attention_layers, 8
+        )
+        self.bond_att = Global_Reactivity_Attention(
+            node_out_feats, attention_heads, attention_layers, 2
+        )
+
         self.pooling_v = nn.Sequential(
-                         nn.Linear(node_out_feats*2, node_out_feats),
-                         self.activation, 
-                         nn.Dropout(0.2),
-                         nn.Linear(node_out_feats, 2))
-        
+            nn.Linear(node_out_feats * 2, node_out_feats),
+            self.activation,
+            nn.Dropout(0.2),
+            nn.Linear(node_out_feats, 2),
+        )
+
         self.pooling_r = nn.Sequential(
-                         nn.Linear(node_out_feats*2, node_out_feats),
-                         self.activation, 
-                         nn.Dropout(0.2),
-                         nn.Linear(node_out_feats, 2))
-        
+            nn.Linear(node_out_feats * 2, node_out_feats),
+            self.activation,
+            nn.Dropout(0.2),
+            nn.Linear(node_out_feats, 2),
+        )
+
         self.bondnet_v = nn.Sequential(
-                         nn.Linear(node_out_feats*2, node_out_feats),
-                         self.activation, 
-                         nn.Linear(node_out_feats, node_out_feats))
-        
+            nn.Linear(node_out_feats * 2, node_out_feats),
+            self.activation,
+            nn.Linear(node_out_feats, node_out_feats),
+        )
+
         self.bondnet_r = nn.Sequential(
-                         nn.Linear(node_out_feats*2, node_out_feats),
-                         self.activation, 
-                         nn.Linear(node_out_feats, node_out_feats))
-        
-        self.output_v =  nn.Sequential(
-                            nn.Linear(node_out_feats, node_out_feats), 
-                            self.activation, 
-                            nn.Dropout(0.2),
-                            nn.Linear(node_out_feats, Template_vn+1))
-        
-        self.output_r =  nn.Sequential(
-                            nn.Linear(node_out_feats, node_out_feats), 
-                            self.activation, 
-                            nn.Dropout(0.2),
-                            nn.Linear(node_out_feats, Template_rn+1))
-        
-        self.poolings = {'virtual': self.pooling_v, 'real': self.pooling_r}
-        self.bondnets = {'virtual': self.bondnet_v, 'real': self.bondnet_r}
-        
+            nn.Linear(node_out_feats * 2, node_out_feats),
+            self.activation,
+            nn.Linear(node_out_feats, node_out_feats),
+        )
+
+        self.output_v = nn.Sequential(
+            nn.Linear(node_out_feats, node_out_feats),
+            self.activation,
+            nn.Dropout(0.2),
+            nn.Linear(node_out_feats, Template_vn + 1),
+        )
+
+        self.output_r = nn.Sequential(
+            nn.Linear(node_out_feats, node_out_feats),
+            self.activation,
+            nn.Dropout(0.2),
+            nn.Linear(node_out_feats, Template_rn + 1),
+        )
+
+        self.poolings = {"virtual": self.pooling_v, "real": self.pooling_r}
+        self.bondnets = {"virtual": self.bondnet_v, "real": self.bondnet_r}
+
     def forward(self, bg, adms, bonds_dict, node_feats, edge_feats):
-        
+
         atom_feats = self.mpnn(bg, node_feats, edge_feats)
         atom_feats, mask = pack_atom_feats(bg, atom_feats)
         atom_feats, atom_attention = self.atom_att(atom_feats, adms, mask)
-        idxs_dict, rout_dict, bonds_feats, bonds = reactive_pooling(bg, atom_feats, bonds_dict, self.poolings, self.bondnets)
+        idxs_dict, rout_dict, bonds_feats, bonds = reactive_pooling(
+            bg, atom_feats, bonds_dict, self.poolings, self.bondnets
+        )
         bond_feats, mask, bcms = pack_bond_feats(bonds_feats, bonds)
         bond_feats, bond_attention = self.bond_att(bond_feats, bcms, mask)
         feats_v, feats_r = unpack_bond_feats(bond_feats, idxs_dict)
         template_v, template_r = self.output_v(feats_v), self.output_r(feats_r)
-        
-        return template_v, template_r, rout_dict['virtual'], rout_dict['real'], idxs_dict['virtual'], idxs_dict['real'], (atom_attention, bond_attention)
+        # template_v: topk x num virtual templates
+        # template_r: topk x num real templates
+        # rout_dict: learned topk bonds x 2
+        # idxs_dict: list of topk indices
+        return (
+            template_v,
+            template_r,
+            rout_dict["virtual"],
+            rout_dict["real"],
+            idxs_dict["virtual"],
+            idxs_dict["real"],
+            (atom_attention, bond_attention),
+        )
+
+
+class ESMLocalTransform(LocalTransform):
+    def __init__(
+        self,
+        node_in_feats,
+        edge_in_feats,
+        node_out_feats,
+        edge_hidden_feats,
+        num_step_message_passing,
+        attention_heads,
+        attention_layers,
+        Template_rn,
+        Template_vn,
+    ):
+        super(ESMLocalTransform, self).__init__(
+            node_in_feats,
+            edge_in_feats,
+            node_out_feats,
+            edge_hidden_feats,
+            num_step_message_passing,
+            attention_heads,
+            attention_layers,
+            Template_rn,
+            Template_vn,
+        )
+        torch.hub.set_dir("/Mounts/rbg-storage1/snapshots/metabolomics/esm2")
+        self.esm_model, self.alphabet = torch.hub.load(
+            "facebookresearch/esm", "esm2_t12_35M_UR50D"
+        )
+        self.esm_tokenizer = self.alphabet.get_batch_converter()
+
+        self.mha = nn.MultiheadAttention(
+            embed_dim=self.esm_model.embed_dim,
+            num_heads=attention_heads,
+            batch_first=True,
+        )
+        self.lin = nn.Linear(2 * node_out_feats, node_out_feats)
+
+    def forward(self, bg, adms, bonds_dict, node_feats, edge_feats, sequences):
+
+        atom_feats = self.mpnn(bg, node_feats, edge_feats)
+
+        # encode protein
+        protein_feats, protein_mask = self.encode_sequence(sequences)
+        dense_cs, cs_mask = pack_atom_feats(bg, atom_feats)  # TODO
+
+        c_protein, _ = (
+            self.mha(  # batch_size, max_num_nodes (reactants), hidden dim # TODO
+                query=dense_cs,
+                key=protein_feats,
+                value=protein_feats,
+                key_padding_mask=protein_mask.float(),
+            )
+        )
+        c_protein = c_protein[cs_mask.bool()]  # TODO
+        atom_feats = self.lin(torch.cat([atom_feats, c_protein], dim=-1))
+        atom_feats, mask = pack_atom_feats(bg, atom_feats)  # TODO
+        atom_feats, atom_attention = self.atom_att(atom_feats, adms, mask)
+        idxs_dict, rout_dict, bonds_feats, bonds = reactive_pooling(
+            bg, atom_feats, bonds_dict, self.poolings, self.bondnets
+        )
+        bond_feats, mask, bcms = pack_bond_feats(bonds_feats, bonds)
+        bond_feats, bond_attention = self.bond_att(bond_feats, bcms, mask)
+        feats_v, feats_r = unpack_bond_feats(bond_feats, idxs_dict)
+        template_v, template_r = self.output_v(feats_v), self.output_r(feats_r)
+        # template_v: topk x num virtual templates
+        # template_r: topk x num real templates
+        # rout_dict: learned topk bonds x 2
+        # idxs_dict: list of topk indices
+        return (
+            template_v,
+            template_r,
+            rout_dict["virtual"],
+            rout_dict["real"],
+            idxs_dict["virtual"],
+            idxs_dict["real"],
+            (atom_attention, bond_attention),
+        )
+
+    def encode_sequence(self, sequences):
+        fair_x = [(i, s) for i, s in enumerate(sequences)]
+        _, _, batch_tokens = self.esm_tokenizer(fair_x)
+        batch_tokens = batch_tokens.to(self.lin.weight.device)
+
+        mask = batch_tokens != self.esm_model.cls_idx
+        mask *= batch_tokens != self.esm_model.padding_idx
+        mask *= batch_tokens != self.esm_model.eos_idx
+
+        result = self.esm_model(
+            batch_tokens,
+            repr_layers=[self.esm_model.num_layers],
+            return_contacts=False,
+        )
+        encoder_hidden_states = result["representations"][self.esm_model.num_layers]
+
+        return encoder_hidden_states, mask
